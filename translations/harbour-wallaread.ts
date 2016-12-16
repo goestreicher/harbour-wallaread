bc2af9425263f4b002de8f4e11e4386ded6e7862
Grégory Oestreicher <greg@kamago.net>
2016-12-16 15:28:12 +0100

    Use a pretty bare cover

--- translations/harbour-wallaread.ts
+++ translations/harbour-wallaread.ts
@@ -4,7 +4,7 @@
 <context>
     <name>CoverPage</name>
     <message>
-        <source>My Cover</source>
+        <source>WallaRead</source>
         <translation type="unfinished"></translation>
     </message>
 </context>

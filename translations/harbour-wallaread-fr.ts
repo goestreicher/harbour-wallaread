9246172f6eba46b39418084758b4d463769e2bdd
Quent-in <pages_quentin@hotmail.com>
2016-12-18 15:38:02 +0100

    French locale

---
+++ translations/harbour-wallaread-fr.ts
@@ -0,0 +1,146 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>WallaRead</source>
+        <translation>WallaRead</translation>
+    </message>
+</context>
+<context>
+    <name>Server</name>
+    <message>
+        <source>Failed to load server information: </source>
+        <translation>Échec du chargement des informations du serveur : </translation>
+    </message>
+    <message>
+        <source>Failed to connect to server: </source>
+        <translation>Échec de la connexion au serveur : </translation>
+    </message>
+    <message>
+        <source>Failed to download articles: </source>
+        <translation>Échec du téléchargement des articles</translation>
+    </message>
+    <message>
+        <source>Failed to set star status on article: </source>
+        <translation>Échec de la mise en favoris de l'article : </translation>
+    </message>
+    <message>
+        <source>Failed to set read status on article: </source>
+        <translation>Échec du passage au statut lu de l'article : </translation>
+    </message>
+    <message>
+        <source>Failed to delete article: </source>
+        <translation>Échec de la suppression de l'article : </translation>
+    </message>
+</context>
+<context>
+    <name>ServerPage</name>
+    <message>
+        <source>Refresh</source>
+        <translation>Actualiser</translation>
+    </message>
+    <message>
+        <source>No articles saved on this server yet</source>
+        <translation>Aucun article enregistré sur le serveur pour l'instant</translation>
+    </message>
+    <message>
+        <source>Deleting</source>
+        <translation>Suppression</translation>
+    </message>
+    <message>
+        <source>Show unstarred articles</source>
+        <translation>Afficher les articles hors favoris</translation>
+    </message>
+    <message>
+        <source>Show only starred articles</source>
+        <translation>Seulement afficher les articles en favoris></translation>
+    </message>
+    <message>
+        <source>Show unread articles</source>
+        <translation>Afficher les articles non lus</translation>
+    </message>
+    <message>
+        <source>Show read articles</source>
+        <translation>Afficher les articles lus</translation>
+    </message>
+</context>
+<context>
+    <name>ServerSettingsDialog</name>
+    <message>
+        <source>Save</source>
+        <translation>Enregistrer</translation>
+    </message>
+    <message>
+        <source>Server Alias</source>
+        <translation>Alias serveur</translation>
+    </message>
+    <message>
+        <source>URL</source>
+        <translation>URL</translation>
+    </message>
+    <message>
+        <source>Server URL</source>
+        <translation>URL du serveur</translation>
+    </message>
+    <message>
+        <source>Login</source>
+        <translation>Login</translation>
+    </message>
+    <message>
+        <source>User Login</source>
+        <translation>Login utilisateur</translation>
+    </message>
+    <message>
+        <source>Client ID</source>
+        <translation>ID du client</translation>
+    </message>
+    <message>
+        <source>Client Secret</source>
+        <translation>Secret du client</translation>
+    </message>
+</context>
+<context>
+    <name>ServersPage</name>
+    <message>
+        <source>Reset database</source>
+        <translation>Réinitialiser la base de données</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Paramètres</translation>
+    </message>
+    <message>
+        <source>Wallabag servers</source>
+        <translation>Serveur Wallabag</translation>
+    </message>
+    <message>
+        <source>No servers configured yet, create your first one with the Settings menu</source>
+        <translation>Aucun serveur configuré, créez-en un dans le menu Paramètres</translation>
+    </message>
+    <message>
+        <source>Resetting database</source>
+        <translation>Réinitialisation de la base de données</translation>
+    </message>
+    <message>
+        <source>Deleting</source>
+        <translation>Suppression</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>New Server</source>
+        <translation>Nouveau serveur</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Paramètres</translation>
+    </message>
+    <message>
+        <source>No server found, use the pulley menu and select &apos;New Server&apos;</source>
+        <translation>Aucun serveur trouvé, utilisez le menu coulissant et sélectionnez &apos;Nouveau serveur&apos;</translation>
+    </message>
+</context>
+</TS>
